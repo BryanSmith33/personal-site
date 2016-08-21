07cd3fb701ae69ed6bfe93a290871ebfc3b5742f
Bryan <b.smith33@hotmail.com>
2016-08-21 01:07:17 -0600

    more changes. start of re-re-style

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var fontArray = ['Above Demo', 'bromello', 'Capsule X Pro Medium', 'Cursus', 'GoodMorningAfternoon', 'Hey Now', 'Permanent Marker'];
+var randomFont = fontArray[Math.floor(Math.random() * fontArray.length)];
+
+$("#name").css("font-family", randomFont);
+console.log(randomFont);
