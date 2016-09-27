9ca7f6e0228f12be4b1722b7e306740ec48359e4
Bryan <b.smith33@hotmail.com>
2016-09-27 14:19:20 -0600

    so many dumb little things

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,16 @@ $(document).on('click','.navbar-collapse.in',function(e) {
     }
 });
 
+$(document).ready(function() {
+
+	$('a').on('click touchend', function(e) {
+		var el = $(this);
+		var link = el.attr('href');
+		window.location = link;
+	});
+
+});
+
 $("#sendMessage").on("click", function() {
   message = $("#contactform").serialize();
 $.ajax({
